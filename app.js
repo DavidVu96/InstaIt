313265eac0503a4a24526fe4a810e278bdb2e506
DavidVu96 <vdnhnguyen96@gmail.com>
2020-09-13 16:15:04 +0100

    change context menu string

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var hp = chrome.contextMenus.create({"title": "InstaIt"});
 
-chrome.contextMenus.create({title:"Find on Instagram for '%s'",contexts: ["all"], "onclick": onRequest});
+chrome.contextMenus.create({title:"Find %s on Instagram",contexts: ["all"], "onclick": onRequest});
 
 function onRequest(info, tab) {
     chrome.tabs.create({url: "https://www.instagram.com/" + info.selectionText + "/"});
